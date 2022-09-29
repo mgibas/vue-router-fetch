faf60f190828e9ca662ba3aeb016938473598c6e
Maciej Gibas <maciej.gibas@infotechinc.com>
2022-09-28 20:57:58 -0400

    fix: install with vue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Guard from './guard.js'
 import { useRouteFetch } from './composables.js'
 
-export default function (router, options) {
-  const guard = new Guard(options)
-  router.beforeEach(guard)
+export default {
+  install: (app, { router, options }) => {
+    const guard = new Guard(options)
+    router.beforeEach(guard)
+  },
 }
 
 export { useRouteFetch }
